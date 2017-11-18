09edfa98bad06dc942acc4aa03de3a12c30c4988
meganzhao <zhaom@carleton.edu>
2017-11-18 14:55:17 +0100

    added floatingaddbutton and alertdialogue

--- app/src/main/java/hu/ait/android/forecast/MainActivity.java
+++ app/src/main/java/hu/ait/android/forecast/MainActivity.java
@@ -1,13 +1,55 @@
 package hu.ait.android.forecast;
 
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fabAdd) FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ButterKnife.bind(this);
+        fabAdd = (FloatingActionButton) findViewById(R.id.fabAdd);
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = createAlertDialog();
+                alertDialog.show();
+            }
+        });
+
+    }
+
+    private AlertDialog createAlertDialog() {
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(getString(R.string.dislogue_title));
+        alertDialog.setContentView(R.layout.alert_dialogue);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.dislogue_positive_botton), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.dislogue_negative_button), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialog.dismiss();
+            }
+        });
+        return alertDialog;
     }
 }
